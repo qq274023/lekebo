9da3af7118cbb3dc67af72b8575eab4959f9a6ed
unknown <274023@qq.com>
2023-04-11 21:24:43 +0800

    Signed-off-by: unknown <274023@qq.com>

---
+++ lekebo_kww.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-04-11 20:38:00
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-04-11 20:38:25
+ * @FilePath: \lekebo\lekebo_kww.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+test
